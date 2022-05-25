d652cee8f6c7266a90b52386e839265b4d0baa6c
daeunYoon <nanuli00@naver.com>
2022-05-25 16:26:31 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { useStateContext } from './contexts/ContextProvider';
 
 
 const App = () => {
-    const { activeMenu, setActiveMenu } = useStateContext();
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
@@ -31,13 +31,13 @@ const App = () => {
                     {activeMenu ? (
                         <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
                             <Sidebar />
-                        </div>) : (<div className='w-0 dark:bg-secondary-dark-bg'><Sidebar /></div>)}
+                        </div>)
+                        : (<div className='w-0 dark:bg-secondary-dark-bg'><Sidebar /></div>)}
                     <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full"><Navbar /></div>
                     </div>
+                    <RouterPath />
                 </div>
-                <RouterPath />
-
             </BrowserRouter>
         </div>
     );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,11 +8,10 @@ import { IoChevronUpOutline } from 'react-icons/io5';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { links } from '../data/dummy';
-
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-    const { activeMenu, setActiveMenu } = useStateContext();
+    const { activeMenu, setActiveMenu, screenSize } = useStateContext();
     const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-wthite text-md m-2';
     const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
     const handleSubCategory = (e) => {
@@ -25,13 +24,19 @@ const Sidebar = () => {
         menuup.classList.toggle('hidden');
     }
 
+    const handleCloseSideBar = () => {
+        if (activeMenu && screenSize <= 900) {
+            setActiveMenu(false)
+        }
+    }
+
     return (
         <div className='ml-3 h-screen
         md:overflow-hidden overflow-auto
         md:hover:overflow-auto pb-10'>
             {activeMenu && (<>
                 <div className="flex justify-between items-center">
-                    <Link to='/' onClick={() => { }}
+                    <Link to='/' onClick={handleCloseSideBar}
                         className='items-center flex gap-3 ml-3 mt-4 text-xl font-extrabold'>
                         <SiShopware /><span>Shoopy</span>
                     </Link>
@@ -52,7 +57,7 @@ const Sidebar = () => {
                                 </p>
                                 <div className='subcategory-cont h-0 overflow-hidden'>
                                     {item.links.map(link => (
-                                        <NavLink to={`/${link.name}`} key={link.name} onClick={() => { }} className={({ isActive }) => isActive ? activeLink : normalLink}>
+                                        <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSideBar} className={({ isActive }) => isActive ? activeLink : normalLink}>
                                             {link.icon}
                                             <span className='capitalize'>{link.name}</span>
                                         </NavLink>
